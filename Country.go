bab4c0ba47b01bb9ad54e8358a9f0636f178f459
unknown <dschenck@win.redhat.com>
2023-07-24 16:27:50 -0400

    Added JSON annotations

--- Country.go
+++ Country.go
@@ -1,10 +1,10 @@
 package main
 
 type Country struct {
-	countryID         int
-	countryName       string
-	population        int
-	landAreaKM        int
-	populationDensity int
+	countryID         int    `json:"countryid"`
+	countryName       string `json:"countryname"`
+	population        int    `json:"population"`
+	landAreaKM        int    `json:"landareakm"`
+	populationDensity int    `json:"populationdensity"`
 }
 type Countries []Country
