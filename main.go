540bdad8c33d5196038e53d1e1122df97f9f6d96
unknown <dschenck@win.redhat.com>
2023-07-24 15:19:19 -0400

    first working copy

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func getCountries(c *gin.Context) {
+func main() {
+	// Create database handle
 	db, err := sql.Open("mysql", "countries:countries@tcp(countries)/countries")
 	defer db.Close()
 
@@ -36,35 +35,4 @@ func getCountries(c *gin.Context) {
 		}
 		countries = append(countries, country)
 	}
-	c.IndentedJSON(http.StatusOK, countries)
-}
-
-func main() {
-	// Create database handle
-	db, err := sql.Open("mysql", "countries:countries@tcp(countries)/countries")
-	defer db.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, err := db.Query("SELECT * FROM countries")
-
-	defer res.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for res.Next() {
-		var country Country
-		err := res.Scan(&country.countryID, &country.countryName, &country.population, &country.landAreaKM, &country.populationDensity)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%v\n", country)
-	}
-
 }
